18d7b86243e275c6bcf4467223332fa43c646746
anasidrissielhassani <anas91idrissi@gmail.com>
2024-04-15 23:52:09 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,7 @@
 /**
  * main - prints "with proper grammar, but the outcome is
  *  a piece of art,"followed by a new line.
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
