90648ed037c6030c373e70ac17ebfdfec418ae7f
anasidrissielhassani <anas91idrissi@gmail.com>
2024-05-20 15:58:23 +0100

    Update and rename holberton.h to main.h

--- 0x13-bit_manipulation/main.h
+++ 0x13-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define HOLBERTON_H
 
 /*
- * File: holberton.h
+ * File: main.h
  * AUth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
