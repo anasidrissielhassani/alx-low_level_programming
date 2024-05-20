724c52c220be2bf3ba4df6e8d5b8a5fd55fb1ef0
anasidrissielhassani <anas91idrissi@gmail.com>
2024-05-20 16:13:22 +0100

    Update main.h

--- 0x13-bit_manipulation/main.h
+++ 0x13-bit_manipulation/main.h
@@ -3,9 +3,7 @@
 
 /*
  * File: main.h
- * AUth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x13-bit_manipulation directory.
+
  */
 
 int _putchar(char c);
